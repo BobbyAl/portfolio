380bfa80bb33c4f7b95e2857140e869b9c7ad5f1
Bobby Flennoy <87404756+BobbyAl@users.noreply.github.com>
2026-04-12 10:57:27 -0500

    feat: initial commit

--- src/components/ui/Tablet.jsx
+++ src/components/ui/Tablet.jsx
@@ -1,6 +1,10 @@
 import { Terminal, Mic, ChevronDown, User, CirclePlay, Camera } from "lucide-react"
+import { useState } from "react";
 
 export default function Tablet() {
+
+    const [search, setSearch] = useState("");
+
     return (
         <div className="flex flex-col w-full max-w-4xl mx-auto gap-8">
             <div className="flex flex-col gap-2">
@@ -12,9 +16,15 @@ export default function Tablet() {
                 bg-gray-300/10 backdrop-blur-sm"
             >
                 <div className="flex justify-between p-8"> 
-                    <div className="flex gap-4">
+                    <div className="flex gap-4 flex-1">
                         <Terminal/>
-                        <span>I am Bobby Flennoy</span>
+                        <input
+                            type="text"
+                            placeholder="Hi, I'm Bobby, ask about my projects..."
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                            className="flex-1 bg-transparent outline-none"
+                        />
                     </div>
                     <div className="flex gap-4">
                         <div className="flex gap-1">
@@ -25,7 +35,7 @@ export default function Tablet() {
                     </div>
                 </div>
             </div>
-            <div className="flex gap-4">
+            <div className="flex gap-12 justify-center">
                 <button className="flex gap-2 px-8 py-4 border border-black rounded-full bg-gray-300/15 backdrop-blur-2xl">
                     <Camera />
                     <span>Photo Gallery</span>
